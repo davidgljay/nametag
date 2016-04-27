8bd829a2976f022cea8e70e924a3500d2699a834
David Jay <davidgljay@gmail.com>
2016-04-27 18:15:12 -0400

    Creating template messages class.

---
+++ app/scripts/ui/Messages.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var React = require('react');
+
+var Messages = React.createClass({
+	getInitialState:function() {
+		
+		return
+	},
+	render:function() {
+		
+		return
+	}
+})
+
+Room.propTypes = { roomid: React.PropTypes.string };
+Room.defaultProps = {roomid:'stampi'};
+
+module.exports=Messages;
