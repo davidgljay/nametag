185b9555560a1a4a8680a018c0fd023c9c405195
David Jay <davidgljay@gmail.com>
2016-05-26 11:10:08 -0700

    Adding login images

--- app/scripts/ui/Participant/Login.js
+++ app/scripts/ui/Participant/Login.js
@@ -15,9 +15,14 @@ FB
 TODO:
 Add Google, Tumblr, Github, LinkedIn, and Instagram
 
+TODO: create e-mail lookup archive and handle account merges
+TODO: Handle login if twitter is already activated
 */
 
-module.exports= function() {
+module.exports= function(props) {
+
+	//TODO: Add e-mail (This will probably involve adding state, oh well.)
+	//TODO: Add FB
 
 	var createUser = function(userinfo) {
 		var fbref = new Firebase(process.env.FIREBASE_URL + 'users/' + userinfo.uid),
@@ -31,6 +36,10 @@ module.exports= function() {
 			//TODO: copy profile image to s3
 			defaultsRef.child('icons').push(userinfo.twitter.profileImageURL);
 		}
+
+		if (userinfo.provider == 'facebook') {
+			console.log(userinfo);
+		}
 	};
 
 	var providerAuth = function(provider) {
@@ -42,8 +51,10 @@ module.exports= function() {
 
 	return (
 		<div id="login">
-			<div className="loginOption" onClick={providerAuth('twitter')}>Twitter</div>
-			<div className="loginOption" onClick={providerAuth('facebook')}>Facebook</div>		
+			<h4>Log in to join</h4>
+			<img src="./images/twitter.jpg" className="loginOption img-circle" onClick={providerAuth('twitter')}/>
+			<img src="./images/fb.jpg" className="loginOption img-circle" onClick={providerAuth('facebook')}/>
+			<img src="./images/tumblr.png" className="loginOption img-circle" onClick={providerAuth('tumblr')}/>	
 		</div>
 		);
 };
