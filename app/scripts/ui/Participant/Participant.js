97cf10cb4713adb16d2025de05b768640ede92c0
David Jay <davidgljay@gmail.com>
2016-06-01 13:49:18 -0400

    Using badges component wherever badges are displayed

--- app/scripts/ui/Participant/Participant.js
+++ app/scripts/ui/Participant/Participant.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var React = require('react');
+var React = require('react'),
+Badges = require('./Badges');
 
 var Participant = React.createClass({
 	render:function() {
@@ -20,11 +21,7 @@ var Participant = React.createClass({
 				<img src={this.props.icon} alt={this.props.name} className="img-circle icon"/>
 				<div className="name">{this.props.name}</div>
 				<div className="bio">{this.props.bio}</div>
-				<div className="badges">
-					{this.props.badges.map(function(badge){
-						return <div className="label label-pill badge" key={badge.name}>{badge.name}</div>
-					})}
-				</div>
+				<Badges/>
 			</div>
 			);
 	}
