137551fc828b849baf16234c2812c3693c00edb2
David Jay <davidgljay@gmail.com>
2016-07-11 17:45:06 -0400

    Switching Navbar bootstrap scheme.

--- app/scripts/ui/Utils/Navbar.js
+++ app/scripts/ui/Utils/Navbar.js
@@ -4,25 +4,25 @@ import style from '../../../styles/Utils/Navbar.css';
 const Navbar = (props) => {
   let login;
   if (props.userAuth) {
-    login = <a className="nav-link" onClick={props.unAuth}>Log out</a>;
+    login = <a className='navbar-link' onClick={props.unAuth}>Log out</a>;
   } else {
-    login = <a className="nav-link" href="#">Log In</a>;
+    login = <a className='navbar-link' href='#'>Log In</a>;
   }
 
-  return <nav className={style.navbar}>
-      <a className={style.navbrand} href="#">Nametag</a>
-      <ul className={style.navlist}>
+  return <nav className='navbar navbar-default'>
+      <a className='navbar-brand' href='#'>Nametag</a>
+      <ul className='nav navbar-nav pull-right'>
         <li>
-          <a href="#">
+          <a href='#'>
             Home
-          <span className={style.sronly}>(current)</span>
+          <span className='sr-only active'>(current)</span>
           </a>
         </li>
         <li>
-          <a href="#">Profile</a>
+          <a href='#'>Profile</a>
         </li>
         <li>
-          <a href="#">About</a>
+          <a href='#'>About</a>
         </li>
         <li>
           {login}
