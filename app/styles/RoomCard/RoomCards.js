fcdeae73dc6d9b7a3a9f40e788c6d2306d4a2150
David Jay <davidgljay@gmail.com>
2016-07-30 17:47:37 -0400

    Switching RoomCards to css-in-js

---
+++ app/styles/RoomCard/RoomCards.js
@@ -0,0 +1,17 @@
+import colors from '../colors'
+
+
+export default {
+  roomSelection: {
+    background: colors.secondary,
+    height: '200vh',
+  },
+  footer: {
+    color: 'grey',
+    fontSize: 11,
+    fontStyle: 'italic',
+    position: 'fixed',
+    bottom: 5,
+    left: 15,
+  },
+}
