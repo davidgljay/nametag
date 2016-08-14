775061269a3d537c359a719190674cb690d612c0
David Jay <davidgljay@gmail.com>
2016-08-13 23:43:47 -0400

    Adding style to gulp file and moving over more bower components

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,62 @@
-'use strict';
+'use strict'
 
-var gulp = require('gulp');
-var del = require('del');
+var gulp = require('gulp')
+var del = require('del')
 
 
-var path = require('path');
+var path = require('path')
 
 
 // Load plugins
-var $ = require('gulp-load-plugins')();
-var browserify = require('browserify');
-var watchify = require('watchify');
+var $ = require('gulp-load-plugins')()
+var browserify = require('browserify')
+var watchify = require('watchify')
 var source = require('vinyl-source-stream'),
 
     sourceFile = './app/scripts/app.js',
 
     destFolder = './dist/scripts',
-    destFileName = 'app.js';
+    destFileName = 'app.js'
 
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
+var browserSync = require('browser-sync')
+var reload = browserSync.reload
 
 // Styles
-gulp.task('styles', ['css-modules']);
+gulp.task('styles', ['css-modules'])
 
 gulp.task('moveCss',['clean'], function(){
   // the base option sets the relative root for the set of files,
   // preserving the folder structure
   gulp.src(['./app/styles/**/*.css'], { base: './app/styles/' })
-  .pipe(gulp.dest('dist/styles'));
-});
+  .pipe(gulp.dest('dist/styles'))
+})
 
 gulp.task('moveIcons',['clean'], function(){
   // the base option sets the relative root for the set of files,
   // preserving the folder structure
   gulp.src(['./app/icons/**/*.svg'], { base: './app/icons/' })
-  .pipe(gulp.dest('dist/icons'));
-});
+  .pipe(gulp.dest('dist/icons'))
+})
 
-gulp.task('sass', function() {
-    return $.rubySass('./app/styles', {
-            style: 'expanded',
-            precision: 10,
-            loadPath: ['app/bower_components']
-        })
-        .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('dist/styles'))
-        .pipe($.size());
-});
+// gulp.task('sass', function() {
+//     return $.rubySass('./app/styles', {
+//             style: 'expanded',
+//             precision: 10,
+//             loadPath: ['app/bower_components']
+//         })
+//         .pipe($.autoprefixer('last 1 version'))
+//         .pipe(gulp.dest('dist/styles'))
+//         .pipe($.size())
+// })
 
 gulp.task('css-modules', function() {
-    var b = browserify();
-    b.add(sourceFile);
+    var b = browserify()
+    b.add(sourceFile)
     b.plugin(['css-modulesify', {
         output:'./dist/styles/main.css',
-    }]);
+    }])
 
-    return b.bundle();
+    return b.bundle()
 })
 
 
@@ -69,10 +69,10 @@ var bundler = watchify(browserify({
     cache: {},
     packageCache: {},
     fullPaths: true,
-}));
+}))
 
-bundler.on('update', rebundle);
-bundler.on('log', $.util.log);
+bundler.on('update', rebundle)
+bundler.on('log', $.util.log)
 
 function rebundle() {
     return bundler.bundle()
@@ -81,19 +81,19 @@ function rebundle() {
         .pipe(source(destFileName))
         .pipe(gulp.dest(destFolder))
         .on('end', function() {
-            reload();
-        });
+            reload()
+        })
 }
 
 // Scripts
-gulp.task('scripts', rebundle);
+gulp.task('scripts', rebundle)
 
 gulp.task('buildScripts', function() {
     return browserify(sourceFile)
         .bundle()
         .pipe(source(destFileName))
-        .pipe(gulp.dest('dist/scripts'));
-});
+        .pipe(gulp.dest('dist/scripts'))
+})
 
 
 
@@ -103,8 +103,8 @@ gulp.task('html', function() {
     return gulp.src('app/*.html')
         .pipe($.useref())
         .pipe(gulp.dest('dist'))
-        .pipe($.size());
-});
+        .pipe($.size())
+})
 
 // Images
 gulp.task('images', function() {
@@ -115,8 +115,8 @@ gulp.task('images', function() {
             interlaced: true
         })))
         .pipe(gulp.dest('dist/images'))
-        .pipe($.size());
-});
+        .pipe($.size())
+})
 
 // Fonts
 gulp.task('fonts', function() {
@@ -124,64 +124,64 @@ gulp.task('fonts', function() {
     return gulp.src(require('main-bower-files')({
             filter: '**/*.{eot,svg,ttf,woff,woff2}'
         }).concat('app/fonts/**/*'))
-        .pipe(gulp.dest('dist/fonts'));
+        .pipe(gulp.dest('dist/fonts'))
     
-});
+})
 
 // Clean
 gulp.task('clean', function(cb) {
-    $.cache.clearAll();
-    cb(del.sync(['dist/styles/*', 'dist/scripts/*', 'dist/images/*', 'dist/icons/*']));
-});
+    $.cache.clearAll()
+    cb(del.sync(['dist/styles/*', 'dist/scripts/*', 'dist/images/*', 'dist/icons/*']))
+})
 
 // Bundle
 gulp.task('bundle', ['styles', 'scripts', 'bower'], function() {
-    return gulp.src('./app/*.html')
-        .pipe($.useref.assets())
-        .pipe($.useref.restore())
-        .pipe($.useref())
-        .pipe(gulp.dest('dist'));
-});
+  return gulp.src('./app/*.html')
+    .pipe($.useref.assets())
+    .pipe($.useref.restore())
+    .pipe($.useref())
+    .pipe(gulp.dest('dist'))
+})
 
 gulp.task('buildBundle', ['styles', 'buildScripts', 'moveLibraries', 'bower'], function() {
-    return gulp.src('./app/*.html')
-        .pipe($.useref.assets())
-        .pipe($.useref.restore())
-        .pipe($.useref())
-        .pipe(gulp.dest('dist'));
-});
+  return gulp.src('./app/*.html')
+    .pipe($.useref.assets())
+    .pipe($.useref.restore())
+    .pipe($.useref())
+    .pipe(gulp.dest('dist'))
+})
 
 // Move JS Files and Libraries
-gulp.task('moveLibraries',['clean'], function(){
+gulp.task('moveLibraries', ['clean'], function() {
   // the base option sets the relative root for the set of files,
   // preserving the folder structure
   gulp.src(['./app/scripts/**/*.js'], { base: './app/scripts/' })
-  .pipe(gulp.dest('dist/scripts'));
-});
+  .pipe(gulp.dest('dist/scripts'))
+})
 
 
 // Bower helper
 gulp.task('bower', function() {
-    gulp.src('app/bower_components/**/*.js', {
-            base: 'app/bower_components'
+    gulp.src('app/bower_components/**/*', {
+            base: 'app/bower_components',
         })
-        .pipe(gulp.dest('dist/bower_components/'));
+        .pipe(gulp.dest('dist/bower_components/'))
 
-});
+})
 
 gulp.task('json', function() {
     gulp.src('app/scripts/json/**/*.json', {
-            base: 'app/scripts'
+            base: 'app/scripts',
         })
-        .pipe(gulp.dest('dist/scripts/'));
-});
+        .pipe(gulp.dest('dist/scripts/'))
+})
 
 // Robots.txt and favicon.ico
 gulp.task('extras', function() {
     return gulp.src(['app/*.txt', 'app/*.ico'])
         .pipe(gulp.dest('dist/'))
-        .pipe($.size());
-});
+        .pipe($.size())
+})
 
 // Watch
 gulp.task('watch', ['html', 'fonts', 'bundle'], function() {
@@ -194,29 +194,29 @@ gulp.task('watch', ['html', 'fonts', 'bundle'], function() {
         //       will present a certificate warning in the browser.
         // https: true,
         server: ['dist', 'app']
-    });
+    })
 
     // Watch .json files
-    gulp.watch('app/scripts/**/*.json', ['json']);
+    gulp.watch('app/scripts/**/*.json', ['json'])
 
     // Watch .html files
-    gulp.watch('app/*.html', ['html']);
+    gulp.watch('app/*.html', ['html'])
 
-    gulp.watch(['app/styles/**/*.scss', 'app/styles/**/*.css'], ['styles', 'scripts', reload]);
+    gulp.watch(['app/styles/**/*.scss', 'app/styles/**/*.css'], ['styles', 'scripts', reload])
 
     
 
     // Watch image files
-    gulp.watch('app/images/**/*', reload);
-});
+    gulp.watch('app/images/**/*', reload)
+})
 
 // Build
 gulp.task('build', ['html', 'buildBundle', 'images', 'fonts', 'extras'], function() {
     gulp.src('dist/scripts/app.js')
         .pipe($.uglify())
         .pipe($.stripDebug())
-        .pipe(gulp.dest('dist/scripts'));
-});
+        .pipe(gulp.dest('dist/scripts'))
+})
 
 // Default task
-gulp.task('default', ['clean', 'build'  , 'jest'  ]);
+gulp.task('default', ['clean', 'build'  , 'jest'  ])
