7689afdcb9a3c1b73de6fe7472f1b891e670e072
David Jay <davidgljay@gmail.com>
2016-08-20 17:01:41 -0400

    Wiring up join button

--- app/scripts/ui/Room/Join.js
+++ app/scripts/ui/Room/Join.js
@@ -1,67 +1,20 @@
 import React, { Component, PropTypes } from 'react'
-import errorLog  from '../../utils/errorLog'
 import Login  from '../User/Login'
 import EditNametag  from '../Nametag/EditNametag'
 import UserCertificates  from '../Certificate/UserCertificates'
 import Alert  from '../Utils/Alert'
-import fbase from '../../api/firebase'
+import {joinRoom} from '../../actions/RoomActions'
 import style from '../../../styles/RoomCard/Join.css'
-
-// TODO: Enable join login buttons via redux
-// TODO: Enable join logged in via redux
 
 
 class Join extends Component {
-  constructor(props) {
-    super(props)
-    // TODO: move this to application state via Redux?
-    this.state = {
-      alert: null,
-      nametag: {
-        name: '',
-        bio: '',
-        icon: '',
-        certificates: [],
-      },
-    }
-  }
 
-  updateUrl() {
-    window.location = '/#/rooms/' + this.props.roomId
+  onJoinClick() {
+    joinRoom(this.props.userNametag)
+      .then(() => {
+        window.location = '/#/rooms/' + this.props.roomId
+      })
   }
-
-  // TODO: Move to room action
-  // joinRoom() {
-  //   let self = this
-  //   if (!this.props.normsChecked) {
-  //     this.setState({
-  //       'alert': 'You must agree to the norms above ' +
-  //       'in order to join this conversation.',
-  //     })
-  //   } else {
-  //     const nametagRef = fbase.child('nametags/' + this.props.roomId)
-  //     if (this.state.nametagId) {
-  //       nametagRef.child(this.state.nametagId)
-  //         .set(this.state.nametag)
-  //         .then(function() {
-  //           self.updateUrl()
-  //         }, errorLog("Joining room "))
-  //     } else {
-  //       nametagRef.push(this.state.nametag)
-  //         .then(function(nametagref) {
-  //           return fbase.child('user_rooms/' + self.context.userAuth.uid + '/' + self.props.roomId)
-  //               .set({
-  //                 mod: false,
-  //                 creator: false,
-  //                 nametag_id: nametagref.key(),
-  //               })
-  //         }, errorLog('Updating user room in Join component'))
-  //         .then(function() {
-  //           self.updateUrl()
-  //         }, errorLog('Joining room '))
-  //     }
-  //   }
-  // }
 
   render() {
     let join
@@ -72,7 +25,6 @@ class Join extends Component {
           <h4>Write Your Nametag For This Conversation</h4>
           <EditNametag
             userNametag={this.props.userNametag}
-            roomId={this.props.roomId}
             dispatch={this.context.dispatch}/>
           <div id={style.userCertificates}>
             <p className={style.userCertificateText}>
@@ -83,7 +35,8 @@ class Join extends Component {
           </div>
           <br/>
           <button
-            className={style.btnPrimary}>
+            className={style.btnPrimary}
+            onClick={this.onJoinClick.bind(this)}>
               Join
           </button>
         </div>
