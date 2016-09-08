4006b89b1d3bc02f69031ffb9751c65eadef870e
David Jay <davidgljay@gmail.com>
2016-09-08 01:02:37 -0400

    Improving room loading lifecycle and adding Compose

--- app/scripts/components/Room/Room.js
+++ app/scripts/components/Room/Room.js
@@ -5,7 +5,7 @@ import {Spinner} from 'react-mdl'
 import Norms from './Norms'
 import Nametags from '../../containers/Nametag/NametagsContainer'
 import Messages from '../../containers/Message/MessagesContainer'
-// import Compose from '../Message/Compose'
+import Compose from '../Message/Compose'
 import style from '../../../styles/Room/Room.css'
 
 class Room extends Component {
@@ -32,8 +32,7 @@ class Room extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if ( nextProps.user.id &&
-    !(this.props.user.nametags && this.props.user.nametags[this.props.params.roomId])) {
+    if (!this.props.userNametag && nextProps.user.id) {
       this.props.getUserNametag(this.props.params.roomId, nextProps.user.id)
     }
   }
@@ -51,55 +50,63 @@ class Room extends Component {
   }
 
   render() {
-    let room = <div className={style.spinner}>
-        <Spinner />
-      </div>
-
     let expanded = this.state.leftBarExpanded ? style.expanded : style.collapsed
-    if (this.props.user.nametags && this.props.user.nametags[this.props.params.roomId]) {
-      room = <div>
-    	    <div className={style.header}>
-                 <span
-                  onClick={this.closeRoom}
-                  className={style.close + ' glyphicon glyphicon-remove'}/>
-                <h3>{this.props.room.title}</h3>
-              <div className={style.description}>
-                {this.props.room.description}
-              </div>
-          </div>
-           <div>
-            <div id={style.leftBar} className={expanded}>
-              <div id={style.leftBarContent}>
-                <div id={style.norms}>
-                  <Norms norms={this.props.room.norms}/>
+    return  <div>
+      {
+        this.props.userNametag && this.props.room ?
+          <div>
+      	    <div className={style.header}>
+                   <span
+                    onClick={this.closeRoom}
+                    className={style.close + ' glyphicon glyphicon-remove'}/>
+                  <h3>{this.props.room.title}</h3>
+                <div className={style.description}>
+                  {this.props.room.description}
                 </div>
-                 <Nametags room={this.props.params.roomId} mod={this.props.room.mod}/>
-              </div>
-              <div id={style.leftBarChevron}>
-                <span
-                  onClick={this.toggleLeftBar.bind(this)}
-                  className={'glyphicon glyphicon-chevron-right'}
-                  />
-              </div>
             </div>
+            <div>
+              <div id={style.leftBar} className={expanded}>
+                <div id={style.leftBarContent}>
+                  <div id={style.norms}>
+                    <Norms norms={this.props.room.norms}/>
+                  </div>
+                   <Nametags room={this.props.params.roomId} mod={this.props.room.mod}/>
+                </div>
+                <div id={style.leftBarChevron}>
+                  <span
+                    onClick={this.toggleLeftBar.bind(this)}
+                    className={'glyphicon glyphicon-chevron-right'}
+                    />
+                </div>
+              </div>
               {
                 <Messages
                 room={this.props.params.roomId}/>
               }
+            </div>
+            {
+              <Compose
+                postMessage={this.props.postMessage}/>
+            }
           </div>
-          {
-          <Compose
-            postRoom={this.props.postRoom}/>
-          }
+          :
+          <div className={style.spinner}>
+            <Spinner />
+          </div>
+        }
       </div>
-    }
-    return room
   }
 }
 
+Room.propTypes = {
+  postMessage: PropTypes.func.isRequired,
+  userNametag: PropTypes.string,
+  room: PropTypes.object,
+}
+
 Room.childContextTypes = {
-  nametagId: PropTypes.string,
-  roomId: PropTypes.string,
+  userNametag: PropTypes.string,
+  room: PropTypes.string,
 }
 
 export default Room
