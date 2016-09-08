4a04b1213bce0fde2f8eefa12a434d4a618c0226
David Jay <davidgljay@gmail.com>
2016-09-08 01:21:11 -0400

    Adding message reducer to reducer index

--- app/scripts/reducers/index.js
+++ app/scripts/reducers/index.js
@@ -3,12 +3,14 @@ import rooms from './RoomReducer'
 import nametags from './NametagReducer'
 import user from './UserReducer'
 import certificates from './CertificateReducer'
+import messages from './MessageReducer'
 
 const mainReducer = combineReducers({
   rooms,
   nametags,
   user,
   certificates,
+  messages,
 })
 
 export default mainReducer
