309da6a56451845b60f7b92fb12df9a5266cdd83
David Jay <davidgljay@gmail.com>
2016-09-10 22:55:44 -0400

    Fixing bug in youtube video display

--- app/scripts/components/Message/Media.js
+++ app/scripts/components/Message/Media.js
@@ -4,15 +4,16 @@ import style from '../../../styles/Message/Media.css'
 
 const Media = (props) => {
   let media
-  if (/youtube\.com/.exec(props.url)) {
-    let ytCode = /youtube\.com\/watch\?v=([a-zA-Z]+)/.exec(props.url)[1]
+  let yt = /youtube\.com\/watch\?v=([a-zA-Z0-9_]+)/.exec(props.url)
+  if (yt) {
+    let ytCode = yt[1]
     media = <div className={style.video}>
         <iframe
           width='560'
           height='315'
           src={'https://www.youtube.com/embed/' + ytCode}
           frameborder='0'
-          allowfullscreen/>;
+          allowfullscreen/>
       </div>
   } else if (/.gif|.png|.jpg/.exec(props.url)) {
     media = <img src={props.url} className={style.mediaImage}/>
