96cc203f48754d70925f7429dc6da3317bc91b5f
David Jay <davidgljay@gmail.com>
2016-09-12 00:04:04 -0400

    Switching MessageMenu to utilize ReactionsContainer

--- app/scripts/components/Message/MessageMenu.js
+++ app/scripts/components/Message/MessageMenu.js
@@ -1,31 +1,10 @@
 import React, {PropTypes} from 'react'
 import style from '../../../styles/Message/MessageMenu.css'
-import EmojiReactions from './EmojiReactions'
-import {Icon} from 'react-mdl'
-
-const wrapperStyle = {
-  height: '2em',
-}
-const selectorStyle = {
-  position: 'absolute',
-  left: 'unset',
-  top: 'unset',
-}
-const xStyle = {
-  color: 'black',
-  marginTop: -45,
-  marginRight: 7,
-}
+import Reactions from '../../containers/Reaction/ReactionsContainer'
 
 const MessageMenu = (props) =>
 <div className={style.actions + ' ' + props.showActions }>
-  <EmojiReactions
-      reactions={[{name: 'blush', count: 1}]}
-      onReaction={this.props.onReaction}
-      onEmojiClick={this.props.onEmojiClick}
-      wrapperStyle={wrapperStyle}
-      selectorStyle={selectorStyle}
-      xStyle={xStyle}/>
+  <Reactions message={props.id} />
   <span
     className={style.showActions + ' ' + style.actionIcon + ' glyphicon glyphicon-option-vertical'}
     onClick={props.toggleActions}
@@ -33,9 +12,6 @@ const MessageMenu = (props) =>
   <span
     className={style.actionIcon + ' glyphicon glyphicon-star'}
     aria-hidden="true"/>
-  <span className={style.actionIcon + ' glyphicon glyphicon-heart'}
-  aria-hidden="true"
-  onClick={props.heartAction}/>
   <span
     className={style.actionIcon + ' glyphicon glyphicon-flag'}
     onClick={props.modAction(true)}
@@ -50,7 +26,5 @@ export default MessageMenu
 
 MessageMenu.propTypes = {
   toggleActions: PropTypes.func.isRequired,
-  heartAction: PropTypes.func.isRequired,
   modAction: PropTypes.func.isRequired,
-
 }
