cbfcd6bd1b7c6bf4a69a5d38826e2fe72483a1cd
David Jay <davidgljay@gmail.com>
2016-09-16 15:34:04 -0400

    Simplifying nabber rendering

--- app/scripts/components/Utils/Navbar.js
+++ app/scripts/components/Utils/Navbar.js
@@ -4,29 +4,21 @@ import {logout} from '../../actions/UserActions'
 import { Layout, Header, Navigation, Drawer } from 'react-mdl'
 
 const Navbar = (props) => {
+  const navigation = <Navigation>
+      {
+      props.user && props.user.id ?
+        <a onClick={() => props.dispatch(logout())}>Log out</a>
+        : <a href='#'>Log In</a>
+      }
+    </Navigation>
   return  <Layout fixedHeader={true}>
-        <Header title="Nametag" style={{color: 'white'}}>
-            <Navigation>
-                {
-                  props.user && props.user.id ?
-                    <a onClick={() => props.dispatch(logout())}>Log out</a>
-                    : <a href='#'>Log In</a>
-                }
-            </Navigation>
-        </Header>
-        <Drawer title="Nametag">
-            <Navigation>
-                <a href='#'>Profile</a>
-                <a href='#'>About</a>
-                {
-                  props.user && props.user.id ?
-                    <a onClick={() => props.dispatch(logout())}>Log out</a>
-                    : <a href='#'>Log In</a>
-                }
-            </Navigation>
-        </Drawer>
-    </Layout>
-
+    <Header title="Nametag" style={{color: 'white'}}>
+      {navigation}
+    </Header>
+    <Drawer title="Nametag">
+      {navigation}
+    </Drawer>
+  </Layout>
 }
 
 export default Navbar
