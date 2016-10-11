495dfa0604efbba985c9b798cc95059e81539316
David Jay <davidgljay@gmail.com>
2016-10-11 17:16:52 -0400

    Adding infrastructure for docker file with node wrapper

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Nametag
+#
+# VERSION               0.0.1
+
+FROM node
+MAINTAINER David Jay <davidgljay@gmail.com>
+
+LABEL description="Used to start the Nametag server"
+LABEL updated="10/9/16"
+RUN apt-get update
+COPY $PWD/.hz /usr/app/.hz
+COPY $PWD/.keys /usr/app/.keys
+COPY $PWD/dist /usr/app/dist
+COPY $PWD/server  /usr/server
+
+WORKDIR usr/server
+RUN npm install
+CMD npm start
---
+++ server/app.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+'use strict'
+
+const express = require('express')
+const horizon = require('@horizon/server')
+const config = require('./secrets.json')
+
+const app = express()
+const httpServer = app.listen(8181)
+const options = {
+  project_name: 'Nametag',
+  rdb_host: 'rethinkdb',
+  // secure: 'yes',
+  // key_file: '/usr/.keys/horizon-key.pem',
+  // cert_file: '/usr/.keys/horizon-cert.pem',
+  // serve_static: 'usr/app/dist',
+  auto_create_collection: 'true',
+  auto_create_index: 'true',
+  auth: {
+    allow_anonymous: false,
+    duration: '30d',
+    allow_unauthenticated: true,
+    success_redirect: '/',
+    create_new_users: true,
+    new_user_group: 'authenticated',
+    token_secret: config.token_secret,
+  },
+}
+const horizonServer = horizon(httpServer, options)
+
+console.log('Listening on port 8181.')
