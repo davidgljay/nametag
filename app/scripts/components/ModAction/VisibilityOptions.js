36b82b43f0087d1bf1d7e0657b7f365d54ef471e
David Jay <davidgljay@gmail.com>
2016-10-16 21:53:58 -0400

    Convert mod action to inline style

--- app/scripts/components/ModAction/VisibilityOptions.js
+++ app/scripts/components/ModAction/VisibilityOptions.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import style from '../../../styles/ModAction/VisibilityOptions.css'
-import { IconButton } from 'react-mdl'
+import IconButton from 'material-ui/IconButton'
+import FontIcon from 'material-ui/FontIcon'
 
-const VisibilityOptions = (props) => <div className={style.chooseVis}>
-    <div className={style.visText}>
+const VisibilityOptions = (props) => <div style={styles.chooseVis}>
+    <div style={styles.visText}>
       {
         props.isPublic ?
         <p>
-          <IconButton name='visibility' onClick={props.setPublic(false)}/>
+          <IconButton onClick={props.setPublic(false)} style={styles.button}>
+            <FontIcon
+              className='material-icons'>
+               visibility
+            </FontIcon>
+          </IconButton>
           Visible to everyone in the room.
         </p>
         :
         <p>
-          <IconButton name='visibility_off' onClick={props.setPublic(true)}/>
+          <IconButton onClick={props.setPublic(true)} style={styles.button}>
+            <FontIcon
+              className='material-icons'>
+               visibility_off
+            </FontIcon>
+          </IconButton>
           Visible only to the author of this message.
         </p>
       }
@@ -20,3 +30,16 @@ const VisibilityOptions = (props) => <div className={style.chooseVis}>
   </div>
 
 export default VisibilityOptions
+
+const styles = {
+  chooseVis: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  visText: {
+    fontStyle: 'italic',
+  },
+  button: {
+    verticalAlign: 'middle',
+  },
+}
