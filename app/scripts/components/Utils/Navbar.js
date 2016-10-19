0c4610200b7e6154ef79b7a501a5e0ac757e44f0
David Jay <davidgljay@gmail.com>
2016-10-19 01:30:18 -0400

    Adding drawer menu

--- app/scripts/components/Utils/Navbar.js
+++ app/scripts/components/Utils/Navbar.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {Component, PropTypes} from 'react'
 import AppBar from 'material-ui/AppBar'
 import FlatButton from 'material-ui/FlatButton'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
 import {indigo500} from 'material-ui/styles/colors'
 import radium from 'radium'
 import {mobile} from '../../../styles/sizes'
@@ -9,24 +11,46 @@ const onCreateRoomClick = () => {
   window.location = '/#/rooms/create'
 }
 
-const Navbar = (props) => {
-  const auth = <div style={styles.buttons}>
-      <FlatButton
-        style={styles.button}
-        onClick={onCreateRoomClick} label='CREATE ROOM'/>
-      {
-      props.user && props.user.id ?
+class Navbar extends Component {
+  state = {
+    open: false
+  }
+
+  render() {
+    const auth = <div style={styles.buttons}>
         <FlatButton
           style={styles.button}
-          onClick={() => props.dispatch(logout())}
-          label='LOG OUT'/>
-        : <FlatButton style={styles.button} label='LOG IN'/>
-      }
+          onTouchTap={onCreateRoomClick} label='CREATE ROOM'/>
+        {
+        this.props.user && this.props.user.id ?
+          <FlatButton
+            style={styles.button}
+            onTouchTap={() => this.props.logout()}
+            label='LOG OUT'/>
+          : <FlatButton style={styles.button} label='LOG IN'/>
+        }
+      </div>
+    return  <div>
+      <AppBar
+        title="Nametag"
+        style = {styles.appBar}
+        iconElementRight={auth}
+        onLeftIconButtonTouchTap={() => this.setState({open: true})}/>
+      <Drawer
+        docked={false}
+        width={200}
+        open={this.state.open}
+        onRequestChange={(open) => this.setState({open})}>
+        <MenuItem onTouchTap={onCreateRoomClick}>Create Room</MenuItem>
+          {
+          this.props.user && this.props.user.id ?
+            <MenuItem
+              onTouchTap={() => this.props.logout()}>Log Out</MenuItem>
+            : <MenuItem>Log In</MenuItem>
+          }
+      </Drawer>
     </div>
-  return  <AppBar
-    title="Nametag"
-    style = {styles.appBar}
-    iconElementRight={auth}/>
+  }
 }
 
 export default radium(Navbar)
