b20ce7dfb291d6e47a0580fe0f71ed29482a3da5
David Jay <davidgljay@gmail.com>
2016-12-29 00:53:18 -0500

    Adding certificate detail component.

--- app/scripts/components/Certificate/CertificateDetail.js
+++ app/scripts/components/Certificate/CertificateDetail.js
@@ -1,6 +1,7 @@
 import React, {Component, PropTypes} from 'react'
 import Certificate from './Certificate'
 import RaisedButton from 'material-ui/RaisedButton'
+import FlatButton from 'material-ui/FlatButton'
 import {indigo500} from 'material-ui/styles/colors'
 import Navbar from '../Utils/Navbar'
 
@@ -23,28 +24,84 @@ class CertificateDetail extends Component {
     //stuff
   }
 
+  onEmailClick = () => {
+
+  }
+
+  onClipboardClick = () => {
+
+  }
+
+  onQRClick = () => {
+
+  }
+
   render() {
     const {user, logout, setting, certificate} = this.props
+
+    let headerText
+    if (certificate) {
+      if (certificate.granted) {
+        headerText = <div style={styles.header}>
+          <h3>This certificate has been claimed.</h3>
+        </div>
+      } else {
+        headerText = certificate.creator !== user.id ?
+        <div>
+          <div style={styles.header}>
+            <h3>Your certificate has been created.</h3>
+            It can be claimed by the first person to visit this URL, please
+            securly share it with the intended recipient of this certificate.
+          </div>
+          <div style={styles.shareButtons}>
+            <FlatButton
+              style={styles.button}
+              onClick={this.onEmailClick(user, certificate)}
+              label='E-MAIL IT'/>
+            <FlatButton
+              style={styles.button}
+              onClick={this.onClipboardClick(user, certificate)}
+              label='COPY TO CLIPBOARD'/>
+            <FlatButton
+              style={styles.button}
+              onClick={this.onQRClick(user, certificate)}
+              label='SHOW A QR CODE'/>
+            </div>
+          </div>
+          : <div style={styles.header}>
+            <h3>You have been granted the following certificate.</h3>
+          </div>
+      }
+    }
+
     return <div>
       <Navbar
         user={user}
         logout={logout}
         setting={setting}/>
       <div style={styles.certDetailContainer}>
+        {
+          headerText
+        }
         <div style={styles.certDetail}>
           <Certificate
             certificate={certificate}
             draggable={false}
             expanded={true}/>
         </div>
-        <div style={styles.claimButton}>
-          <RaisedButton
-            style={styles.button}
-            labelStyle={styles.buttonLabel}
-            backgroundColor={indigo500}
-            onClick={this.onClaimClick(user, certificate)}
-            label='CLAIM'/>
-        </div>
+        {
+          certificate &&
+          !certificate.granted &&
+          certificate.creator !== user.id &&
+          <div style={styles.claimButton}>
+            <RaisedButton
+              style={styles.button}
+              labelStyle={styles.buttonLabel}
+              backgroundColor={indigo500}
+              onClick={this.onClaimClick(user, certificate)}
+              label='CLAIM THIS CERTIFICATE'/>
+          </div>
+        }
       </div>
     </div>
   }
@@ -53,6 +110,10 @@ class CertificateDetail extends Component {
 export default CertificateDetail
 
 const styles =  {
+  header: {
+    width: 450,
+    textAlign: 'center',
+  },
   certDetailContainer: {
     display: 'flex',
     alignItems: 'center',
@@ -60,5 +121,16 @@ const styles =  {
   },
   certDetail: {
     fontSize: 16,
+    lineHeight: '20px',
+  },
+  claimButton: {
+    margin: 30,
+  },
+  buttonLabel: {
+    color: '#fff',
+  },
+  shareButtons: {
+    display: 'flex',
+    justifyContent: 'space-between',
   },
 }
