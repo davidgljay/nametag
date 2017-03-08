aa2098905e57285c9efb9f131c1c9723676e9334
David Jay <davidgljay@gmail.com>
2017-03-07 19:12:17 -0500

    Posting notifications from server.

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,2 +1,3 @@
 module.exports.LAMBDA_RESIZE_URL = 'https://cl3z6j4irk.execute-api.us-east-1.amazonaws.com/prod/image_resize'
 module.exports.LAMBDA_UPLOAD_URL = 'https://cl3z6j4irk.execute-api.us-east-1.amazonaws.com/prod/image'
+module.exports.GCM_NOTIF_URL = 'https://fcm.googleapis.com/fcm/send'
--- server/src/listeners/notifications.js
+++ server/src/listeners/notifications.js
@@ -1,4 +1,7 @@
 const r = require('../../horizon/server/src/horizon.js').r
+const fetch = require('node-fetch')
+const config = require('../secrets.json')
+const db = db
 
 const onMessage = (conn) => (err, message) => {
   if (err) {
@@ -6,7 +9,7 @@ const onMessage = (conn) => (err, message) => {
     return Promise.reject(err)
   }
 
-  return r.db('nametag').table('user_nametags').filter({room: message.new_val.room})
+  return db.table('user_nametags').filter({room: message.new_val.room})
     .update({latestMessage: message.new_val.timestamp}).run(conn)
     .then(() => checkMentions(message, conn))
 }
@@ -17,9 +20,10 @@ const checkMentions = (message, conn) => {
 
   const room = message.new_val.room
 
-  return r.db('nametag').table('nametags').filter({room}).run(conn)
+  return db.table('nametags').filter({room}).run(conn)
   .then((roomNametags) => {
     const splitMsg = message.new_val.text.split('@')
+    const from = message.new_val.author
     let promises = []
     // For every mention, check every nametag in the room to see if it matches the name.
     roomNametags.toArray((err, nametags) => {
@@ -29,7 +33,10 @@ const checkMentions = (message, conn) => {
         for (let j = 0; j < nametags.length; j++) {
           const {name, id} = nametags[j]
           if (text.slice(0, name.length).toLowerCase() === name.toLowerCase()) {
-            promises.push(addMention(id, room, conn))
+            promises.push(
+              addMention(id, room, conn)
+              .then(() => postMention(id, from, room, 'MENTION', conn))
+            )
           }
         }
       }
@@ -40,18 +47,67 @@ const checkMentions = (message, conn) => {
   })
 }
 
-const addMention = (nametag, room, conn) => r.db('nametag').table('user_nametags')
+const addMention = (nametag, room, conn) => db.table('user_nametags')
 .filter({room, nametag}).update({mentions: r.row('mentions').prepend(Date.now())}).run(conn)
 
+const postMention = (to, from, room, reason, conn) => Promise.all([
+  db.table('user_nametags').filter({room, nametag:to}).run(conn)
+    .then({user} => rb.table('users').get(user)),
+  db.table('rooms').get(room),
+  db.table('nametags').get(from)
+])
+.then([user, room, from] => {
+  postFCMNotif({
+    reason,
+    room: {
+      title: room.title,
+      id: room.id,
+      image: room.image
+    },
+    from: {
+      name: from.name,
+      icon: from.icon
+    }
+  }, user.fcmToken)
+})
+
 module.exports = {
-  messageNotifs: (conn) => r.db('nametag').table('messages').changes().run(conn)
+  messageNotifs: (conn) => db.table('messages').changes().run(conn)
       .then((feed) => feed.each(onMessage(conn))),
-  dmNotifs: (conn) => r.db('nametag').table('direct_messages').changes().run(conn)
+  dmNotifs: (conn) => db.table('direct_messages').changes().run(conn)
       .then((feed) => feed.each((err, dm) => {
+        const {recipient, author, room} = dm.new_val;
         if (err) {
           console.error(err)
           return Promise.reject(err)
         }
-        return addMention(dm.new_val.recipient, dm.new_val.room, conn)
+        return addMention(recipient, room, conn)
+        .then(() => postMention(recipient, author, room, 'DM', conn))
       }))
+}
+
+const postFCMNotif = (data, token) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `key=${config.fcm.key}`
+    },
+    body: JSON.stringify({
+      data,
+      to: token
+    })
+  }
+
+  if (!token) {
+    return Promise.reject(new Error('Cannot send message, user does not have a fcm token'))
+  }
+  console.log('Posting notif', options);
+
+  return fetch(GCM_NOTIF_URL, options)
+    .then((res) => {
+      console.log('res ok', res.json);
+      return res.ok ? res.json()
+        : Promise.reject(new Error(res.statusCode))
+    })
 }
