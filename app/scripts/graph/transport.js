e85a5e21da973a2a83fbf1d317c4e851625275f6
David Jay <davidgljay@gmail.com>
2017-03-26 00:45:47 -0400

    Fixing websocket URL in development.

--- app/scripts/graph/transport.js
+++ app/scripts/graph/transport.js
@@ -1,6 +1,10 @@
 import {createNetworkInterface} from 'apollo-client'
 import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 
+const websocketURL = process.env.NODE_ENV === 'production'
+  ? `wss://${window.location.hostname}`
+  : `wss://${window.location.hostname}:8181`
+
 export default function getNetworkInterface (headers = {}) {
   return addGraphQLSubscriptions(
     new createNetworkInterface({ //eslint-disable-line
@@ -10,7 +14,7 @@ export default function getNetworkInterface (headers = {}) {
         headers
       }
     }),
-    new SubscriptionClient(`wss://${window.location.hostname}`, {
+    new SubscriptionClient(websocketURL, {
       reconnect: true
     })
   )
