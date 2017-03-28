d166048a658a3d12ea04ff005a7a96dc1cce664e
David Jay <davidgljay@gmail.com>
2017-03-28 19:34:46 -0400

    Adding template to nametagInput schema.

--- server/src/graph/resolvers/RootQuery.js
+++ server/src/graph/resolvers/RootQuery.js
@@ -3,7 +3,7 @@ const RootQuery = {
   room: (obj, {id}, {models: {Rooms}}) => Rooms.get(id),
   me: (obj, args, {user}) =>  user,
   granter: (obj, {id}, {models: {BadgeGranters}}) => BadgeGranters.get(id),
-  badge: (obj, {id}, {models: {Badges}}) => Badges.get(id)
+  template: (obj, {id}, {models: {BadgeTemplates}}) => BadgeTemplates.get(id)
 }
 
 module.exports = RootQuery
