0228446a03d514a83510e20e80ccf1bb8e7aec70
David Jay <davidgljay@gmail.com>
2017-03-31 14:02:50 +0200

    Enabling granter name in badge detail.

--- app/scripts/components/Room/RoomCards.js
+++ app/scripts/components/Room/RoomCards.js
@@ -96,7 +96,7 @@ export default RoomCards
 
 const styles = {
   roomCards: {
-    background: "#fbfbfb",
+    background: '#fbfbfb',
     paddingBottom: 50,
     paddingTop: 20,
     minHeight: '100vh',
