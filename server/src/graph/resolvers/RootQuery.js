3b0ce1d07e12b3140b885588d466745485de089f
David Jay <davidgljay@gmail.com>
2017-04-20 15:45:41 -0400

    Addressing case where user has no badges.

--- server/src/graph/resolvers/RootQuery.js
+++ server/src/graph/resolvers/RootQuery.js
@@ -3,7 +3,7 @@ const {ErrNotAuthorized, ErrNotLoggedIn} = require('../../errors')
 const RootQuery = {
   rooms: (obj, args, {user, models: {Rooms}}) => Promise.all([
     Rooms.getPublic(),
-    user ? Rooms.getByTemplates(Object.keys(user.badges), true) : []
+    user && user.badges ? Rooms.getByTemplates(Object.keys(user.badges), true) : []
   ])
   .then(([pub, priv]) => pub.concat(priv).sort((a, b) => b.createdAt - a.createdAt)),
   room: (obj, {id}, {models: {Rooms}}) => Rooms.get(id),
