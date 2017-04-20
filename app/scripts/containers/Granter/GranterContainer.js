4b551bb023461de7a8153a983f6efe7476f00b3d
David Jay <davidgljay@gmail.com>
2017-04-20 14:33:53 -0400

    Adding login to granter page when user is not logged in.

--- app/scripts/containers/Granter/GranterContainer.js
+++ app/scripts/containers/Granter/GranterContainer.js
@@ -1,9 +1,20 @@
 import component from '../../components/Granter/Granter'
+import {connect} from 'react-redux'
 import {compose} from 'react-apollo'
+import {registerUser, loginUser} from '../../actions/UserActions'
 import {createBadge, updateBadgeRequestStatus, addNote} from '../../graph/mutations'
 import {granterQuery} from '../../graph/queries'
 
+const mapDispatchToProps = (dispatch) => {
+  const disp = (func) => (...args) => dispatch(func.apply(this, args))
+  return {
+    registerUser: disp(registerUser),
+    loginUser: disp(loginUser)
+  }
+}
+
 const Granter = compose(
+  connect(() => ({}), mapDispatchToProps),
   createBadge,
   addNote,
   updateBadgeRequestStatus,
