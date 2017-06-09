ad14f7e3566ca83e035cc64177d35087cd266695
David Jay <davidgljay@gmail.com>
2017-06-09 00:14:51 -0700

    Fixing bug with new room creation.

--- server/src/graph/subscriptions/Rooms.js
+++ server/src/graph/subscriptions/Rooms.js
@@ -10,11 +10,14 @@ const RoomSubscription = conn => db.table('rooms').changes().run(conn)
         errorLog(new APIError('Error in room subscription feed'))
         return
       }
+      if (!room.new_val) {
+        return
+      }
       const roomForIndex = room.new_val.templates.length === 0
         ? Object.assign({}, room.new_val, {templates: ['public']})
         : room.new_val
       index(roomForIndex, 'room', 'room')
-      if (room.old_val.latestMessage !== room.new_val.latestMessage) {
+      if (room.old_val && room.old_val.latestMessage !== room.new_val.latestMessage) {
         pubsub.publish('latestMessageUpdated', {
           latestMessage: room.new_val.latestMessage,
           roomId: room.new_val.id
