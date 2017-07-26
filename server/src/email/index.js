6f6c497f3558a834c39a8d351b9da5baa7f9d9a6
Max Goodman <c@chromakode.com>
2017-07-25 18:19:26 -0700

    Skip sending emails if no Sendgrid key available

--- server/src/email/index.js
+++ server/src/email/index.js
@@ -33,6 +33,11 @@ module.exports = ({to, from, template, params}) => {
     ]
   }
 
+  if (!sendgrid.key) {
+    console.log(`No sendgrid API key set. Skipping sending email to ${to}: ${JSON.stringify(mail)}`)
+    return Promise.resolve()
+  }
+
   const request = sg.emptyRequest({
     method: 'POST',
     path: '/v3/mail/send',
