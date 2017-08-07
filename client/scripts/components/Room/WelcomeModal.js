15af8bf3af86ed6cb7b0b56998b018d2f70d9204
David Jay <davidgljay@gmail.com>
2017-08-07 12:21:59 -0700

    Creating basic welcome modal.

--- client/scripts/components/Room/WelcomeModal.js
+++ client/scripts/components/Room/WelcomeModal.js
@@ -1,27 +1,72 @@
 import React, {PropTypes} from 'react'
 import Dialog from 'material-ui/Dialog'
+import Nametag from '../Nametag/Nametag'
+import {Card} from 'material-ui/Card'
+import Compose from '../Message/Compose'
 
-const WelcomeDialog = ({postMessage, welcome, nametags, showWelcome, toggleWelcome}) =>
+const WelcomeModal = ({
+  createMessage,
+  welcome,
+  nametags,
+  mod,
+  myNametag,
+  showWelcome,
+  toggleWelcome,
+  updateNametag
+}) =>
   <Dialog
     modal={false}
     contentStyle={styles.dialog}
     open={showWelcome}
-    onRequestClose={() => toggleWelcome()} />
+    onRequestClose={() => toggleWelcome()}>
+    <div style={styles.cardsContainer}>
+      {
+        nametags.map(nametag =>
+          <Card key={nametag.id} id={nametag.id} style={styles.card}>
+            <Nametag
+              nametag={nametag}
+              hideDMs
+              myNametagId={myNametag.id}
+              mod={mod}
+              />
+          </Card>
+        )
+      }
+    </div>
+    <h3>{welcome}</h3>
+  </Dialog>
 
-const {func, string, arrayOf, object, bool} = PropTypes
+const {func, string, arrayOf, object, bool, shape} = PropTypes
 
-WelcomeDialog.propTypes = {
-  postMessage: func.isRequired,
+WelcomeModal.propTypes = {
+  createMessage: func.isRequired,
   welcome: string.isRequired,
   showWelcome: bool.isRequired,
   toggleWelcome: func.isRequired,
-  nametags: arrayOf(object).isRequired
+  nametags: arrayOf(object).isRequired,
+  mod: object.isRequired,
+  roomId: string.isRequired,
+  updateNametag: func.isRequired,
+  myNametag: shape({id: string.isRequired}).isRequired
 }
 
-export default WelcomeDialog
+export default WelcomeModal
 
 const styles = {
   dialog: {
-    width: 250
+    maxWidth: 800
+  },
+  card: {
+    width: 240,
+    minHeight: 60,
+    margin: 5,
+    padding: 5
+  },
+  cardsContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    maxHeight: '50vh',
+    overflowY: 'auto',
+    paddingBottom: 30
   }
 }
