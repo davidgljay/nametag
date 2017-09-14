0d3538ab27137942b10622fe294ef1136b237dbb
David Jay <davidgljay@gmail.com>
2017-09-13 22:57:26 -0700

    Fixing structure of typingPromptAction.

--- client/scripts/actions/TypingPromptActions.js
+++ client/scripts/actions/TypingPromptActions.js
@@ -8,11 +8,13 @@ import constants from '../constants'
 * Adds a typing prompt and sets a timer to remove that typing prompt unless
 * another one is received from the server.
 */
-export const addTypingPrompt = (nametagId) => (dispatch) => {
-  return {
-    type: constants.ADD_TYPING_PROMPT,
-    nametagId,
-    timer: setTimeout(() => dispatch(removeTypingPrompt(nametagId)), constants.TYPING_PROMPT_DELAY)
+export function addTypingPrompt (nametagId) {
+  return dispatch => {
+    dispatch({
+      type: constants.ADD_TYPING_PROMPT,
+      nametagId,
+      timer: setTimeout(() => dispatch(removeTypingPrompt(nametagId)), constants.TYPING_PROMPT_DELAY)
+    })
   }
 }
 
