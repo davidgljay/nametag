6a043d60a2a73686e40b9cab3a53a349b522bb51
David Jay <davidgljay@gmail.com>
2017-09-20 14:08:40 -0700

    Adding automatic reply in demo.

--- server/src/graph/subscriptions/Messages.js
+++ server/src/graph/subscriptions/Messages.js
@@ -2,7 +2,7 @@ const pubsub = require('./pubsub')
 const {APIError, errorLog} = require('../../errors')
 const {db} = require('../../db')
 
-const MessageSubscription = ({conn, models: {Rooms}}) => db.table('messages').changes().run(conn)
+const MessageSubscription = ({conn, models: {Rooms, Messages}}) => db.table('messages').changes().run(conn)
   .then(feed => {
     feed.each((err, message) => {
       if (err) {
@@ -12,6 +12,13 @@ const MessageSubscription = ({conn, models: {Rooms}}) => db.table('messages').ch
       if (!message.old_val) {
         // Send a new room message notification if necessary
         Rooms.notifyOfNewMessage(message.new_val.room)
+        setTimeout(() => {
+          Messages.create({
+            author:  "9b3177dd-778b-4032-a306-c902fcec20df" ,
+            room:  "40bed376-768c-4a03-8e62-285f5a426b13" ,
+            text:  "Thanks for joining! I think many of us can relate to that experience."
+          }), 2000})
+
       }
       if (!message.new_val) {
         pubsub.publish('messageDeleted', message.old_val)
