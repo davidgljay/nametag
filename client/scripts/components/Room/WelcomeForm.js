9987c0940d0a75b39f063f83090d7ad053937990
David Jay <davidgljay@gmail.com>
2017-09-25 16:21:33 -0700

    Updating bio on welcome form.

--- client/scripts/components/Room/WelcomeForm.js
+++ client/scripts/components/Room/WelcomeForm.js
@@ -1,49 +1,67 @@
-import React, {PropTypes} from 'react'
+import React, {PropTypes, Component} from 'react'
 import Nametag from '../Nametag/Nametag'
 import {Card} from 'material-ui/Card'
 import Compose from '../Message/Compose'
 
-const onPost = (updateNametag, onWelcomeMsgSent, myNametagId) => (post) => {
-  updateNametag(myNametagId, {bio: post})
-  onWelcomeMsgSent()
-}
+class WelcomeForm extends Component {
 
-const WelcomeForm = ({
-  createMessage,
-  welcome,
-  nametags,
-  mod,
-  defaultMessage,
-  roomId,
-  myNametag,
-  updateNametag,
-  onWelcomeMsgSent
-}) =>
-  <div className='welcome'>
-    <h3 style={styles.header}>{welcome}</h3>
-    <Compose
-      roomId={roomId}
-      myNametag={myNametag}
-      createMessage={createMessage}
-      defaultMessage={defaultMessage}
-      mod={mod}
-      topic=''
-      onPost={onPost(updateNametag, onWelcomeMsgSent, myNametag.id)}
-      />
-    <div style={styles.cardsContainer}>
-      {
-        nametags.map(nametag =>
-          <Card key={nametag.id} id={nametag.id} style={styles.card}>
-            <Nametag
-              nametag={nametag}
-              hideDMs
-              myNametagId={myNametag.id}
-              modId={mod.id} />
-          </Card>
-        )
-      }
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      bio: ''
+    }
+
+    this.onPost = (post) => {
+      const {updateNametag, onWelcomeMsgSent, myNametagId} = this.props
+      updateNametag(myNametagId, {bio: post})
+      onWelcomeMsgSent()
+    }
+
+    this.onUpdateText = (bio) => this.setState({bio})
+  }
+
+  render () {
+    const {
+        createMessage,
+        welcome,
+        nametags,
+        mod,
+        defaultMessage,
+        roomId,
+        myNametag
+      } = this.props
+
+    return <div className='welcome'>
+      <h3 style={styles.header}>{welcome}</h3>
+      <Compose
+        roomId={roomId}
+        myNametag={myNametag}
+        createMessage={createMessage}
+        defaultMessage={defaultMessage}
+        mod={mod}
+        topic=''
+        onPost={this.onPost}
+        onUpdateText={this.onUpdateText}
+        />
+      <div style={styles.cardsContainer}>
+        {
+          nametags.map(nt => {
+            const nametag = nt.id === myNametag.id ? {...nt, bio: this.state.bio} : nt
+            return <Card key={nametag.id} id={nametag.id} style={styles.card}>
+              <Nametag
+                nametag={nametag}
+                hideDMs
+                myNametagId={myNametag.id}
+                modId={mod.id} />
+            </Card>
+          }
+          )
+        }
+      </div>
     </div>
-  </div>
+  }
+}
 
 const {func, string, arrayOf, object, shape} = PropTypes
 
