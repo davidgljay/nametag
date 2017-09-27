9b1ae350bac52187b016d0f57c550a548093a4e8
David Jay <davidgljay@gmail.com>
2017-09-27 12:59:26 -0700

    Updating email styling.

--- server/src/email/mention.js
+++ server/src/email/mention.js
@@ -27,7 +27,7 @@ module.exports = ({roomId, roomName, message, author, userToken}) => ({
     <tr>
     <td style="text-align: center">
         <a href="https://nametag.chat/rooms/${roomId}" target='_blank'>
-            <div style="margin-top: 10px; margin-left: auto; margin-right: auto; padding: 10px; font-size: 20px; background: #12726a; color: #FFF; border-radius: 3px; text-decoration: none; max-width: 150px; text-align: center;">
+            <div style="margin-top: 10px; margin-left: auto; margin-right: auto; padding: 10px; font-size: 20px; background: #12726a; color: #FFF; border-radius: 3px; text-decoration: none !important; max-width: 150px; text-align: center;">
                 Reply
             </div>
         </a>
--- server/src/email/templates.js
+++ server/src/email/templates.js
@@ -2,10 +2,12 @@ const passwordReset = require('./password-reset')
 const emailConfirm = require('./email-confirm')
 const badgeRequest = require('./badge-request')
 const mention = require('./mention')
+const publicRoom = require('./public-room')
 
 module.exports = {
   passwordReset,
   emailConfirm,
   badgeRequest,
-  mention
+  mention,
+  publicRoom
 }
