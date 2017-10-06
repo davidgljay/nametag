0c77e0ef714359dfe6a7f6b3c5596d37c32e7537
David Jay <davidgljay@gmail.com>
2017-10-05 22:54:40 -0700

    Adding privacy policy and Tos.

---
+++ client/scripts/components/Static/Privacy.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import Navbar from '../Utils/Navbar'
+
+const PrivacyPolicy = () => <div>
+  <Navbar empty />
+  <div style={styles.container}>
+    <h1>Nametag Privacy Policy</h1>
+    <h3>1. Your Acceptance</h3>
+    <div>By using or visiting the Nametag, Inc. (Nametag) website or any Nametag products, software, data feeds, and services provided to you on, from, or through the Nametag website (collectively the "Service") you signify your agreement to this privacy policy (the "Privacy Policy").  If you do not agree to any of these terms, then please do not use the Service.</div>
+    <div>Although we may attempt to notify you when major changes are made to this Privacy Policy, you should periodically review the most up-to-date version which is posted at  https:\/\/nametag.chat.  Nametag may, in its sole discretion, modify or revise this Privacy Policy and other policies at any time, and you agree to be bound by such modifications or revisions.  Nothing in this Privacy Policy shall be deemed to confer any third-party rights or benefits.</div>
+    <h3>2. Scope of this Statement</h3>
+    <div>This Statement covers the “personal data and information” we collect through this Service. "Personal data and information" is data that identifies you personally, either alone or in combination with other information available to us. Examples of personal information include your name, address, e-mail address, tax identification number and the information you provide when you use our Site to create an account or a personal profile.</div>
+    <h3>3. Data Collected</h3>
+    <div>Personally Identifiable Information refers to information that lets us know the specifics of who you are. Personal information is requested when you order a Service, register for an account and send us feedback. For example, this information might include your name, address (both billing and registered), e-mail address, industry and product details, and credit card or other payment information. Such information may be held on our contacts and administration databases either because of the work we have done with you or are currently engaged in, or because we believe you might be interested in receiving material from us about our business and services. This allows us to manage our relationship with you efficiently, improve our Site and the services which we offer to you and to target items of interest we may send to you.</div>
+    <div>Non-Personally Identifiable Information refers to information that is statistical in nature only and that does not, by itself, identify a specific individual or entity, and which depends upon your usage of a Service. For example, this information may include the domain from which you access the Internet, the date and time you access the website, and the Internet address of the website from which you linked directly to the website. This information is used in an aggregate form to make the Service more useful and attractive to its users.</div>
+    <div>Agreements Governing Your Use of Special Features and Password-Protected Areas of a Service. A Service may include special features and password-protected areas. For example, you may need a username and password to access areas of our websites where you may purchase Nametag products.</div>
+    <div>When you sign up to use a special feature or password-protected area, you may be asked to agree to special terms governing your use of the special feature or password-protected area. In such cases, you will be asked to expressly consent to the special terms, for example, by checking a box or clicking on a button marked "I agree." This type of agreement is known as a "click-through" agreement. If any of the terms of the click-through agreement are different than the terms of this policy, the terms of the click-through agreement will supplement or amend this Statement, but only with respect to the matters governed by the "click-through agreement."</div>
+    <div>Cookies and Similar Technologies. We use "cookies" and other Web technologies to collect information and support certain features of our sites. For example, we may use these technologies to collect information about the ways visitors use a Service – which website pages they visit, which links they use, and how long they stay on each page, for the following reasons:
+  to manage and determine the effectiveness of our advertising campaigns;
+  to support the features and functionality of a Service – for example, to save you the trouble of reentering information already in our database or to prompt the settings you established on previous visits to our sites; and
+  to personalize your experience when you use a Service.</div>
+    <div>Generally, the information we collect using these Web technologies does not identify you personally. If, however, you have created a user identity, for example, by signing up to use a password-protected area on one of our sites, we may link the information we collect using Web technologies to other information that identifies you personally.</div>
+    <div>If you do not wish to receive cookies, you may set your browser to reject cookies or to alert you when a cookie is placed on your computer. You may also delete our cookies as soon as you leave a site. Although you are not required to accept our cookies when you visit our website, if you set your browser to reject cookies, you will not be able to use all of the features and functionality of our sites.</div>
+    <h3>4. Data Uses</h3>
+    <div>Generally. We use the personal information we collect through our sites
+<ul>
+  <li>to provide the products, information and services you request;</li>
+  <li>for security, credit or fraud prevention purposes;</li>
+  <li>to provide you with effective customer service;</li>
+  <li>to provide you with a personalized experience;</li>
+  <li>to contact you with information and notices;</li>
+  <li>to invite you to participate in surveys and provide feedback to us;</li>
+  <li>to better understand your needs and interests;</li>
+  <li>to improve the content, functionality and usability of a Service;</li>
+  <li>to improve our products and services;</li>
+  <li>to improve our marketing and promotional efforts; and</li>
+  <li>for any other purpose identified in an applicable privacy notice or other agreement between Nametag and you.</li>
+</ul>
+    </div>
+    <h3>5. Data Sharing</h3>
+    <div>Service Providers. We share personal information with companies and organizations that perform services on our behalf, for example, companies that provide support services to us (such as credit card processing services, data storage services, and web hosting services) or that help us market our products and services (such as third-party e-mail marketing companies). These third parties are required by contract to use the personal information we share with them only to perform services on our behalf and to take commercially reasonable measures to protect the confidentiality and security of your personal information.</div>
+    <div>With Our Business Partners. Under some limited circumstances, we may share our mailing lists or other personal information collected through a Service with companies that are not our service providers and are not members of the Nametag family of businesses.</div>
+    <div>Business Transfers. Your personal information may be transferred to a company that has acquired the equity or assets of Nametag, one of its affiliates, or one of our businesses, for example, as the result of a sale, merger, reorganization, or liquidation. If such a transfer occurs, the acquiring company{'\''}s use of your personal information will still be subject to this policy and the privacy preferences you have expressed to us.</div>
+    <div>Compliance With Laws and Protection of Our Rights and the Rights of Others. We may disclose personal information when we, in good faith, believe disclosure is appropriate to comply with the law, a court order or a subpoena. We may also disclose personal information to prevent or investigate a possible crime, such as fraud or identity theft; to enforce or apply our online terms of use or other agreements; or to protect our own rights or property or the rights, property or safety of our users or others.</div>
+    <div>Aggregate Information. We may share aggregated demographic or survey information with third parties, but this information is in anonymous form and does not contain any personal data or information. The aggregate information that we share may include anonymous information that is captured through the use of cookies and other similar tracking technology.</div>
+    <div>As Described in a Privacy Notice. We reserve the right to disclose your personal information as described in any privacy notice posted on the Nametag website where you provide that information. By providing your personal information on that Web page you will be consenting to the disclosure of your personal information as described in that privacy notice.</div>
+    <div>As Described in a Click-Through Agreement. We reserve the right to disclose your personal information as described in any click-through agreement to which you have agreed.</div>
+    <h3>6. Updating Personal Information</h3>
+    <div>If you have created an account or a personal profile on a Service, you may be able to access and update some or all of your personal information on your account settings or profile page.</div>
+    <h3>7. Security</h3>
+    <div>We maintain a comprehensive data security program, including commercially reasonable administrative, physical, and technological security measures, to protect your personal information from unauthorized access, unauthorized use, and unauthorized or accidental destruction, modification or disclosure. Although we take commercially reasonable precautions to provide a level of security appropriate to the sensitivity of the personal information we collect, we cannot guarantee the security of information provided over the Internet or stored in our databases.</div>
+    <h3>8. Password Protection</h3>
+    <div>For your protection, certain areas of a Service may be password protected. You are responsible for maintaining the confidentiality of your passwords. We have the right to assume that anyone accessing a Service using a password assigned to you has the right to do so. You will be solely responsible for the activities of anyone accessing our sites using a password assigned to you, even if the individual is not, in fact authorized by you. If you have reason to believe that your password has been compromised or used without authorization, you must promptly change it using the functionality provided on the Service.</div>
+    <h3>9. Links to Third-Party Websites.</h3>
+    <div>This site may contain links to websites operated by other companies, including websites operated by our third-party service providers, our affiliates, and other third parties. This policy does not apply to personal information collected on any of these third-party websites. When you access third-party websites through a link on this site, you are responsible to know and comply with the policies of that website.</div>
+    <h3>10. Children</h3>
+    <div>Our Site is not directed nor targeted to children under the age of thirteen (13). We do not use our Site to knowingly solicit data from or market to children under the age of thirteen (13). If you are under the age of thirteen (13), do not provide us with any personal information. If we learn that someone under thirteen (13) has provided personally identifiable information through one of our sites, we will use reasonable efforts to remove such information from our databases.</div>
+    <h3>11. Governing Law</h3>
+    <div>This Statement is governed under the laws of the state of California without regard to its conflicts of law provisions.</div>
+  </div>
+</div>
+
+export default PrivacyPolicy
+
+const styles = {
+  container: {
+    margin: 50,
+    fontWeight: 300
+  }
+}
