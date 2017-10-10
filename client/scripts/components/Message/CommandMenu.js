a5086a2a3ad1d720649959a1801849d18687b810
David Jay <davidgljay@gmail.com>
2017-10-09 22:37:44 -0700

    Adding announce command to command menu.

--- client/scripts/components/Message/CommandMenu.js
+++ client/scripts/components/Message/CommandMenu.js
@@ -24,6 +24,11 @@ const CommandMenu = ({isMod, setDefaultMessage, onRequestClose, open, anchor}) =
       command: 'title',
       mod: true,
       description: 'Update this room\'s title'
+    },
+    {
+      command: 'announce',
+      mod: true,
+      description: 'Send an e-mail to everyone in the room'
     }
   ]
 
