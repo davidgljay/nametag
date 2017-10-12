3acaedea19c93a48a77fe055e6c0f9dbdb810d6d
David Jay <davidgljay@gmail.com>
2017-10-11 18:10:31 -0700

    Adding additional automated message for demo.

--- server/src/graph/subscriptions/Messages.js
+++ server/src/graph/subscriptions/Messages.js
@@ -12,7 +12,7 @@ const MessageSubscription = ({conn, models: {Rooms, Messages}}) => db.table('mes
       if (!message.old_val) {
         // Send a new room message notification if necessary
         Rooms.notifyOfNewMessage(message.new_val.room)
-        if (message.new_val.text.match('green')) {
+        if (message.new_val.text.match('developer')) {
         setTimeout(() => {
             Messages.create({
               author:  "9b3177dd-778b-4032-a306-c902fcec20df" ,
@@ -20,6 +20,14 @@ const MessageSubscription = ({conn, models: {Rooms, Messages}}) => db.table('mes
               text:  "Thanks for joining! I think many of us can relate to that experience."
             })
           }, 5000)
+        }
+        setTimeout(() => {
+            Messages.create({
+              author:  "9b3177dd-778b-4032-a306-c902fcec20df" ,
+              room:  "40bed376-768c-4a03-8e62-285f5a426b13" ,
+              text:  "@David Jay I'd love to invite you to join our member community, we have regular conversations about these issues."
+            })
+          }, 8000)
         }
       }
       if (!message.new_val) {
