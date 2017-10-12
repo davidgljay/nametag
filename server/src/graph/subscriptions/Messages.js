fe10f2ae273a9739bdad8da0aa78c4e57165a007
David Jay <davidgljay@gmail.com>
2017-10-12 01:17:44 -0400

    Fixing typo in Messages.

--- server/src/graph/subscriptions/Messages.js
+++ server/src/graph/subscriptions/Messages.js
@@ -13,22 +13,21 @@ const MessageSubscription = ({conn, models: {Rooms, Messages}}) => db.table('mes
         // Send a new room message notification if necessary
         Rooms.notifyOfNewMessage(message.new_val.room)
         if (message.new_val.text.match('founder')) {
-        setTimeout(() => {
-            Messages.create({
-              author:  "9b3177dd-778b-4032-a306-c902fcec20df" ,
-              room:  "40bed376-768c-4a03-8e62-285f5a426b13" ,
-              text:  "@DJ Welcome! Would love to hear your perspective."
-            })
-          }, 5000)
-        }
-        setTimeout(() => {
-            Messages.create({
-              author:  "9b3177dd-778b-4032-a306-c902fcec20df" ,
-              room:  "40bed376-768c-4a03-8e62-285f5a426b13" ,
-              template: "0485f7b6-1b57-4884-9e79-29911f1377aa",
-              text:  "@David Jay I'd love to invite you to join our member community, we have regular conversations about these issues."
-            })
-          }, 9000)
+          setTimeout(() => {
+              Messages.create({
+                author:  "9b3177dd-778b-4032-a306-c902fcec20df" ,
+                room:  "40bed376-768c-4a03-8e62-285f5a426b13" ,
+                text:  "@DJ Welcome! Would love to hear your perspective."
+              })
+            }, 5000)
+          setTimeout(() => {
+              Messages.create({
+                author:  "9b3177dd-778b-4032-a306-c902fcec20df" ,
+                room:  "40bed376-768c-4a03-8e62-285f5a426b13" ,
+                template: "0485f7b6-1b57-4884-9e79-29911f1377aa",
+                text:  "@David Jay I'd love to invite you to join our member community, we have regular conversations about these issues."
+              })
+            }, 9000)
         }
       }
       if (!message.new_val) {
