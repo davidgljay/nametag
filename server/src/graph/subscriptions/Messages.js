7ec005f2feeec99447f8d6e8310da791cf641327
David Jay <davidgljay@gmail.com>
2017-10-12 01:13:52 -0400

    Adding template to demo message.

--- server/src/graph/subscriptions/Messages.js
+++ server/src/graph/subscriptions/Messages.js
@@ -12,12 +12,12 @@ const MessageSubscription = ({conn, models: {Rooms, Messages}}) => db.table('mes
       if (!message.old_val) {
         // Send a new room message notification if necessary
         Rooms.notifyOfNewMessage(message.new_val.room)
-        if (message.new_val.text.match('developer')) {
+        if (message.new_val.text.match('founder')) {
         setTimeout(() => {
             Messages.create({
               author:  "9b3177dd-778b-4032-a306-c902fcec20df" ,
               room:  "40bed376-768c-4a03-8e62-285f5a426b13" ,
-              text:  "Thanks for joining! I think many of us can relate to that experience."
+              text:  "@DJ Welcome! Would love to hear your perspective."
             })
           }, 5000)
         }
@@ -25,9 +25,10 @@ const MessageSubscription = ({conn, models: {Rooms, Messages}}) => db.table('mes
             Messages.create({
               author:  "9b3177dd-778b-4032-a306-c902fcec20df" ,
               room:  "40bed376-768c-4a03-8e62-285f5a426b13" ,
+              template: "0485f7b6-1b57-4884-9e79-29911f1377aa",
               text:  "@David Jay I'd love to invite you to join our member community, we have regular conversations about these issues."
             })
-          }, 8000)
+          }, 9000)
         }
       }
       if (!message.new_val) {
