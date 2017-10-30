c4629314b7111714b14810974057a9568a9ab327
David Jay <davidgljay@gmail.com>
2017-10-29 23:31:59 -0700

    Removing unised cron directory.

--- server/src/cron/digest.js
+++
@@ -1,9 +0,0 @@
-const r = require('rethinkdb')
-const Context = require('../graph/context')
-
-r.connect({host: 'rethinkdb'})
-  .then(conn => {
-    console.log('Connecting to rethinkDB')
-    const context = new Context(conn)
-    context.models.Users.emailDigest()
-  })
