12d3a2879aba9c09e3ed6478aaf766e7b0efd166
David Jay <davidgljay@gmail.com>
2017-11-08 21:11:15 -0800

    Supporting images over both http and https.

--- server/src/routes/images/imageRedirect.js
+++ server/src/routes/images/imageRedirect.js
@@ -1,7 +1,9 @@
 const http = require('http')
+const https = require('https')
 
-module.exports.redirect = (url, response) =>
-  http.get(url, res => {
+module.exports.redirect = (url, response) => {
+  const protocol = url.slice(0,5) === 'https' ? https : http
+  protocol.get(url, res => {
     response.setHeader('Content-Type', res.headers['content-type'])
     response.setHeader('Content-Length', res.headers['content-length'])
     res.on('data', chunk => {
@@ -11,3 +13,4 @@ module.exports.redirect = (url, response) =>
       response.end()
     })
   })
+}
