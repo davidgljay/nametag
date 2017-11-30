c3f375185ea951f3319ccf98104a618cfe8fd115
David Jay <davidgljay@gmail.com>
2017-11-29 22:48:06 -0800

    Fixing bug in room subscriptions.

--- server/src/graph/subscriptions/Rooms.js
+++ server/src/graph/subscriptions/Rooms.js
@@ -40,7 +40,7 @@ const RoomSubscription = ({conn, models: {Nametags, Users}}) => db.table('rooms'
             email({
               to: user.email,
               from: {name: 'Nametag', email: 'noreply@nametag.chat'},
-              template: 'approvedRoom',
+              template: 'roomApproval',
               params: {
                 roomId: room.new_val.id,
                 roomTitle: room.new_val.title,
