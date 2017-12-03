ef248114c41b6a08d25af2293e234ad129b39225
David Jay <davidgljay@gmail.com>
2017-12-02 16:29:51 -0800

    Updating message template resolver now that get functions return null when passed null.

--- server/src/graph/resolvers/Message.js
+++ server/src/graph/resolvers/Message.js
@@ -1,7 +1,7 @@
 const Message = {
   author: ({author}, args, {models: {Nametags}}) => Nametags.get(author),
   room: ({room}, args, {models: {Rooms}}) => Rooms.get(room),
-  template: ({template}, args, {models: {Templates}}) => template ? Templates.get(template): null,
+  template: ({template}, args, {models: {Templates}}) => Templates.get(template),
   replies: ({id}, {limit}, {models: {Messages}}) => Messages.getReplies(id, limit),
   replyCount: ({id}, {limit}, {models: {Messages}}) => Messages.getReplyCount(id),
   parent: ({parent}, args, {models: {Messages}}) => Messages.get(parent),
