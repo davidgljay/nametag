5f51c43565289d3daaea91b85339e86f7fc76978
David Jay <davidgljay@gmail.com>
2017-12-04 22:15:28 -0800

    Updating notification on reply and removing optimistic message posting, which seems to trigger an error.

--- client/scripts/graph/mutations/index.js
+++ client/scripts/graph/mutations/index.js
@@ -266,41 +266,41 @@ export const createMessage = graphql(CREATE_MESSAGE, {
       variables: {
         message
       },
-      optimisticResponse: {
-        createMessage: {
-          __typename: 'CreateMessageResponse',
-          message: {
-            __typename: 'Message',
-            id: `tempMessage_${Date.now()}`,
-            text: message.text,
-            createdAt: new Date().toISOString(),
-            editedAt: null,
-            saved: false,
-            replies: [],
-            replyCount: 0,
-            author: {
-              __typename: 'Nametag',
-              image: author.image,
-              id: author.id,
-              name: author.name
-            },
-            recipient: null,
-            reactions: [],
-            nametag: null,
-            parent: message.parent ? {
-              __typename: 'Message',
-              id: message.parent,
-              author: {
-                __typename: 'Nametag',
-                id: 'tempAuthor',
-                name: ''
-              }
-            }
-            : null
-          },
-          errors: null
-        }
-      },
+      // optimisticResponse: {
+      //   createMessage: {
+      //     __typename: 'CreateMessageResponse',
+      //     message: {
+      //       __typename: 'Message',
+      //       id: `tempMessage_${Date.now()}`,
+      //       text: message.text,
+      //       createdAt: new Date().toISOString(),
+      //       editedAt: null,
+      //       saved: false,
+      //       replies: [],
+      //       replyCount: 0,
+      //       author: {
+      //         __typename: 'Nametag',
+      //         image: author.image,
+      //         id: author.id,
+      //         name: author.name
+      //       },
+      //       recipient: null,
+      //       reactions: [],
+      //       nametag: null,
+      //       parent: message.parent ? {
+      //         __typename: 'Message',
+      //         id: message.parent,
+      //         author: {
+      //           __typename: 'Nametag',
+      //           id: 'tempAuthor',
+      //           name: ''
+      //         }
+      //       }
+      //       : null
+      //     },
+      //     errors: null
+      //   }
+      // },
       updateQueries: {
         roomQuery: (oldData, {mutationResult: {data: {createMessage: {message, errors}}}}) => {
           if (errors) {
