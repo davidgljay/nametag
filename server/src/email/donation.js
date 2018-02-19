9127e9029d7bae835c9c2653e609c9d2576a0bf4
David Jay <davidgljay@gmail.com>
2018-02-19 13:08:47 -0800

    Updating display in donation e-mail.

--- server/src/email/donation.js
+++ server/src/email/donation.js
@@ -1,6 +1,6 @@
 module.exports = ({roomId, roomTitle, donorName, donorImage, donorEmail, amount}) => ({
   subject: `${donorName} has donated in ${roomTitle}`,
-  txt: `${donorName} has donated $${amount}!
+  txt: `${donorName} has donated $${amount/100}!
   \n
   Their e-mail is: ${donorEmail}\n
   \n
@@ -14,8 +14,11 @@ module.exports = ({roomId, roomTitle, donorName, donorImage, donorEmail, amount}
     </td></tr>
 <tr>
     <td style="text-align: center;">
-    ${donorImage ? `<div style="width: 50px; height: 50px; border-radius: 25px; background: url(${donorImage})   center center / cover no-repeat;"/>` : ''}
-    <h2>${donorName} has donated $${amount}!</h2></td>
+      ${donorImage ? `<div style="width: 50px; height: 50px; border-radius: 25px; background: url(${donorImage})   center center / cover no-repeat;"/>` : ''}
+    </td>
+    <td style="text-align: center;">
+      <h2>${donorName} has donated $${amount/100}!</h2>
+    </td>
 </tr>
 <tr>
     <td style="padding: 10px; margin: 5px; border: 2px #a8a8a8 solid; min-width: 250px; border-radius: 3px;">
