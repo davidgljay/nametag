bfdc3dd04063a578f709c33f2add80b10a497dc2
David Jay <davidgljay@gmail.com>
2018-02-25 17:42:52 -0800

    Updating sharebuttons with room shortLink.

--- client/scripts/components/Room/ShareButtons.js
+++ client/scripts/components/Room/ShareButtons.js
@@ -1,24 +1,24 @@
 import React, {PropTypes} from 'react'
 import t from '../../utils/i18n'
 
-const getUrl = id => encodeURI(`https://nametag.chat/rooms/${id}`)
+const getUrl = shortLink => encodeURI(`https://nametag.chat/r/${shortLink}`)
 
-const twitterLink = (roomId, title) => `https://twitter.com/intent/tweet?text=${encodeURI(t('room.invite_twitter') + title)}&url=${getUrl(roomId)}&via=NametagChat`
+const twitterLink = (shortLink, title) => `https://twitter.com/intent/tweet?text=${encodeURI(t('room.invite_twitter') + title)}&url=${getUrl(shortLink)}&via=NametagChat`
 
-const fbLink = (roomId, title) => `https://www.facebook.com/dialog/share?app_id=672526412895563&display=page&href=${getUrl(roomId)}&redirect_uri=${getUrl(roomId)}`
+const fbLink = (shortLink, roomId, title) => `https://www.facebook.com/dialog/share?app_id=672526412895563&display=page&href=${getUrl(shortLink)}&redirect_uri=${encodeURI(`https://nametag.chat/rooms/${roomId}`)}`
 
-const emailLink = (roomId, title) => `mailto:?subject=${encodeURI(title)}&body=${encodeURI(t('room.invite_email'))}${getUrl(roomId)}`
+const emailLink = (shortLink, title) => `mailto:?subject=${encodeURI(title)}&body=${encodeURI(t('room.invite_email'))}${getUrl(shortLink)}`
 
-const ShareButtons = ({roomId, title}) => <div style={styles.container}>
+const ShareButtons = ({shortLink, title, roomId}) => <div style={styles.container}>
   <div style={styles.header}>{t('room.invite')}</div>
   <div id='twitterShare' style={styles.share}>
-    <a href={twitterLink(roomId, title)} target='_blank'><img style={styles.image} src='https://s3.amazonaws.com/nametag_images/white-twitter-30.png' /></a>
+    <a href={twitterLink(shortLink, title)} target='_blank'><img style={styles.image} src='https://s3.amazonaws.com/nametag_images/white-twitter-30.png' /></a>
   </div>
   <div id='fbShare' style={styles.share}>
-    <a href={fbLink(roomId, title)} target='_blank'><img style={styles.image} src='https://s3.amazonaws.com/nametag_images/facebook-logo-white-30.png' /></a>
+    <a href={fbLink(shortLink, roomId, title)} target='_blank'><img style={styles.image} src='https://s3.amazonaws.com/nametag_images/facebook-logo-white-30.png' /></a>
   </div>
   <div id='emailShare' style={styles.share}>
-    <a href={emailLink(roomId, title)} target='_blank'><img style={styles.image} src='https://s3.amazonaws.com/nametag_images/icon_email_30.png' /></a>
+    <a href={emailLink(shortLink, title)} target='_blank'><img style={styles.image} src='https://s3.amazonaws.com/nametag_images/icon_email_30.png' /></a>
   </div>
 </div>
 
@@ -26,7 +26,8 @@ const {string} = PropTypes
 
 ShareButtons.proptypes = {
   roomId: string.isRequired,
-  title: string.isRequired
+  title: string.isRequired,
+  shortLink: string.isRequired
 }
 
 export default ShareButtons
