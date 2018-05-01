5e3ec18713f42fbb84e709d871bc6b76666be027
David Jay <davidgljay@gmail.com>
2018-04-30 20:46:14 -0700

    Making oembed responsive.

--- server/src/routes/granters/embed.js
+++ server/src/routes/granters/embed.js
@@ -5,20 +5,17 @@ module.exports = (req, res, next) => {
   res.render('embed.pug', {title: 'Nametag Embed'})
 }
 
-module.exports.oEmbed = (req, res, next, {conn, models: {Granters}}) => {
-  console.log(req.params.granter)
-  return Granters.getByUrlCode(req.params.granter)
+module.exports.oEmbed = (req, res, next, {conn, models: {Granters}}) =>
+  Granters.getByUrlCode(req.params.granter)
   .then(granter => granter
     ? db.table('rooms').getAll(granter.id, {index: 'granter'}).count().run(conn)
     : 0
   )
   .then(count => {
     res.json({
-        version: 1,
-        type: 'HTML',
+        version: '1.0',
+        type: 'rich',
         height: 170 * count,
-        width: '100%',
         html: pug.renderFile('/usr/nametag/server/views/embed.pug')
     })
   })
-}
